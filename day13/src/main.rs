01e6d381692d1e0ce08ab08c351d3f0f819d8551
Björn Ingeson <bjorn@avassa.io>
2022-12-16 15:29:34 +0100

    day13 not quite there

---
+++ day13/src/main.rs
@@ -0,0 +1,295 @@
+use std::fs;
+use std::convert::From;
+use std::cmp::Ordering;
+use std::cmp::Ordering::Equal;
+use std::iter::zip;
+
+fn main() {
+    let input = fs::read_to_string("day13/input.txt").unwrap();
+    println!("{}", one(&input));
+    // println!("{}", two(&input));
+}
+
+#[derive(Clone, Debug)]
+enum Packet {
+    List(Vec<Packet>),
+    Num(u8),
+}
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self {
+            Packet::Num(l) => match other {
+                Packet::Num(r) => {
+                    println!("num {} {}", l, r);
+                    l.cmp(r)
+                }
+                Packet::List(_) => {
+                    let mut v = Vec::new();
+                    v.push(self.clone());
+                    Packet::List(v).cmp(&other)
+                }
+            }
+            Packet::List(l) => match other {
+                Packet::Num(_) => {
+                    let mut v = Vec::new();
+                    v.push(other.clone());
+                    self.cmp(&Packet::List(v))
+                }
+                Packet::List(r) => {
+                    for (a, b) in zip(l, r) {
+                        match (*a).cmp(b) {
+                            Equal => continue,
+                            ret => {
+                                // println!("diff\n{:?}\n{:?}\n", a, b);
+                                return ret
+                            }
+                        }
+                    }
+                    println!("len");
+                    l.len().cmp(&r.len())
+                }
+            }
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let i = self.cmp(other);
+        println!("{:?}", i);
+        Some(i)
+    }
+}
+
+impl PartialEq for Packet {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Equal
+    }
+}
+
+impl Eq for Packet { }
+
+#[derive(Debug)]
+struct Pair {
+    left: Packet,
+    right: Packet,
+}
+
+impl Pair {
+    fn is_correct(&self) -> bool {
+        self.left < self.right
+    }
+}
+
+impl From<&str> for Pair {
+    fn from(value: &str) -> Self {
+        let mut packets = value.split_whitespace();
+        let mut l_iter = packets.next().unwrap_or("").chars();
+        let mut r_iter = packets.next().unwrap_or("").chars();
+        let left = parse(&mut l_iter);
+        let right = parse(&mut r_iter);
+
+        Pair {
+            left,
+            right,
+        }
+
+    }
+}
+
+fn parse<T>(input: &mut T) -> Packet
+where
+    T: IntoIterator + Iterator<Item = char>
+{
+    let mut p: Vec<Packet> = Vec::new();
+    let mut num: String = String::new();
+
+    while let Some(c) = input.next() {
+        match c {
+            '[' => p.push(parse(input)), 
+            ']' => {
+                match num.parse::<u8>() {
+                    Ok(n) => {
+                        p.push(Packet::Num(n));
+                        num.clear();
+                    }
+                    Err(_) => continue,
+                }
+                return Packet::List(p)
+            }
+            ',' => {
+                match num.parse::<u8>() {
+                    Ok(n) => {
+                        p.push(Packet::Num(n));
+                        num.clear();
+                    }
+                    Err(_) => continue,
+                }
+            }
+            n => num.push(n),
+        };
+    }
+    Packet::List(p)
+}
+
+fn one(input: &str) -> usize {
+    let pairs: Vec<Pair> = input.split("\n\n").map(Pair::from).collect();
+    
+    pairs.into_iter().enumerate()
+        // .inspect(|(i, p)| println!("Testing {} {:?}", i, p))
+        .filter(|(_, p)| p.is_correct())
+        // .inspect(|(i, p)| println!("Ok {} {:?}", i, p))
+        .map(|(i, _)| i + 1).sum()
+}
+
+// fn two() -> usize {
+
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cmp::Ordering::{Greater, Less};
+
+    const INPUT: &str = "\
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+";
+
+const LT: &str = "\
+[0]
+[1]
+
+[1]
+[2]
+
+[1]
+[2,1]
+
+[9,1]
+[10]
+
+[[2]]
+[[3]]
+
+[[2]]
+[[2],1]
+
+[[2],2]
+[[2],3]
+
+[1,1]
+[1,2]
+
+[]
+[[]]
+
+[]
+[[2]]
+
+[[[]],1]
+[[[]],2]
+";
+
+const EQ: &str = "\
+[]
+[]
+
+[[1],2]
+[1,[2]]
+
+[1]
+[1]
+
+[1,2]
+[1,2]
+
+[[2]]
+[[2]]
+
+[[],1]
+[[],1]
+
+[1,[]]
+[1,[]]
+
+[1]
+[[[1]]]
+
+[1]
+[[1]]
+
+[[1]]
+[1]
+";
+
+const TT: &str = "\
+[[[],5]]
+[[[4,7,[10,10,5,9,2]],10],[0,[[1,4,1,4],[3,7,2,8,0],6,[],1],[[],[0,4,6]],[]],[[]],[2]]
+";
+
+    #[test]
+    fn test_tt() {
+        let pairs: Vec<Pair> = TT.split("\n\n").map(Pair::from).collect();
+        for p in pairs {
+            assert_eq!(p.left.cmp(&p.right), Less);
+        }
+    }
+
+    #[test]
+    fn test_one() {
+        assert_eq!(one(INPUT), 13);
+    }
+
+    #[test]
+    fn test_eq() {
+        let pairs: Vec<Pair> = EQ.split("\n\n").map(Pair::from).collect();
+        for p in pairs {
+            assert_eq!(p.left.cmp(&p.right), Equal);
+            assert_eq!(p.right.cmp(&p.left), Equal);
+        }
+    }
+    #[test]
+    fn test_lt() {
+
+        let pairs: Vec<Pair> = LT.split("\n\n").map(Pair::from).collect();
+        for p in pairs {
+            assert_eq!(p.left.cmp(&p.right), Less);
+        }
+    }
+
+    #[test]
+    fn test_gt() {
+        let pairs: Vec<Pair> = LT.split("\n\n").map(Pair::from).collect();
+        for p in pairs {
+            assert_eq!(p.right.cmp(&p.left), Greater);
+        }
+    }
+
+    // #[test]
+    // fn test_two() {
+    //     assert_eq!(two(INPUT), )
+    // }
+}
